3500699e6e9de3269c7f01ab5432a333660189ed
Ashutosh Maurya <92643685+ashu1230@users.noreply.github.com>
2022-11-30 19:51:17 +0530

    Update index.py

--- Contact-Management-System-In-PYTHON-master/index.py
+++ Contact-Management-System-In-PYTHON-master/index.py
@@ -3,7 +3,7 @@ import sqlite3
 import tkinter.ttk as ttk
 import tkinter.messagebox as tkMessageBox
 
-#DEVELOPED BY Mark Arvin
+#DEVELOPED BY Ashutosh Maurya
 root = Tk()
 root.title("Contact List")
 width = 700
